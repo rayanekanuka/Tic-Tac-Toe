261fa77c439e52f644768c1dc6e309da95ac37e7
rayanekanuka <rayanekanuka@gmail.com>
2024-11-18 10:20:43 +0100

    Display board done

--- src/Cell.java
+++ src/Cell.java
@@ -1,3 +1,12 @@
 public class Cell {
+    private String representation;
     
+    public Cell() {
+        this.representation = " ";
+    }
+
+    public String getRepresentation() {
+        return representation;
+    }
+
 }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,27 @@
 public class TicTacToe {
+    private int size = 3;
+    private Cell[][] board;
+
+    public TicTacToe() {
+        board = new Cell[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                board[i][j] = new Cell();
+            }
+        }
+    }
+
+    public void display() {
+        for (int i = 0; i < size; i++) {
+            System.out.print("|"); // Début de ligne
+            for (int j = 0; j < size; j++) {
+                System.out.print(board[i][j].getRepresentation() + " |");
+            }
+            System.out.println(""); // Nouvelle ligne après une rangée
+            if (i < size - 1) {
+                System.out.println("----------"); // Séparateurs horizontaux
+            }
+        }
+    }
 
 }
