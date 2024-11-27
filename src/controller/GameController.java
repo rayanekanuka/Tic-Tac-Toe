74477dfaf03976825d59e7c73276b50c9198ae66
rayanekanuka <rayanekanuka@gmail.com>
2024-11-27 15:08:25 +0100

    GameController on the way

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -1,10 +1,19 @@
 package controller;
 
+import model.games.Game;
+import view.GameView;
+
 public abstract class GameController {
 
-
     public GameController() {
+        protected Game game;
+        protected GameView gameView;
 
+    public GameController(Game game, GameView gameView) {
+        this.game = game;
+        this.gameView = gameView;
     }
+
+    
 
 }
